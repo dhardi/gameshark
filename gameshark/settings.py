6f323b8da0726aa54337297930c87394b40a859a
Denis Hardi <denishardi10@outlook.com>
2024-09-01 11:15:12 +0000

    added checkout views and templates

--- gameshark/settings.py
+++ gameshark/settings.py
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +67,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gameshark.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 TEMPLATES = [
@@ -79,8 +85,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
+            
         },
     },
 ]
