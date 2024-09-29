4c9b615e6117bec4363b6a8bbab8aef43c749cd3
Denis Hardi <denishardi10@outlook.com>
2024-09-29 08:50:07 +0000

    added testimonial on django admin

--- testimonial/admin.py
+++ testimonial/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Testimonial
 
-# Register your models here.
+# Customização da administração do Testimonial
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'rating', 'order', 'created_at')  # Campos a serem exibidos na lista
+    search_fields = ('user__username', 'text')  # Campos pelos quais é possível buscar
+    list_filter = ('rating', 'created_at')  # Filtros laterais
+    ordering = ('-created_at',)  # Ordena por data de criação (mais recente primeiro)
+    readonly_fields = ('created_at',)  # Campos apenas para leitura
+
+# Registrar o modelo no admin
+admin.site.register(Testimonial, TestimonialAdmin)
