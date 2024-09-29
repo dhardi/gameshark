b7a29c77be908a18959883e915a3402431b949fc
Denis Hardi <denishardi10@outlook.com>
2024-09-29 11:13:02 +0000

    added view index

--- home/views.py
+++ home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
+from testimonial.models import Testimonial
+from products.models import Product, Category  # Certifique-se de que o modelo de Category está importado
 
 def index(request):
     """ A view to return the index page """
+    
+    # Buscando todas as avaliações
+    testimonials = Testimonial.objects.all()
+    
+    # Buscando a categoria 'Deals'
+    deals_category = Category.objects.get(name="deals")  # Substitua "Deals" pelo nome exato da categoria
+    hot_deals = Product.objects.filter(category=deals_category)
 
-    return render(request, 'home/index.html')
+    context = {
+        'testimonials': testimonials,
+        'hot_deals': hot_deals,
+    }
 
+    return render(request, 'home/index.html', context)
