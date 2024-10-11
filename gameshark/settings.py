5ee7fe7ae85eb0b21f30f49ca8b7ec9bc5f1d64c
Denis Hardi <denishardi10@outlook.com>
2024-10-11 14:49:23 +0000

    debug false

--- gameshark/settings.py
+++ gameshark/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-dhardi-gameshark-bhu4l2pphf2.ws.codeinstitute-ide.net','gameshark-4e405cfc132a.herokuapp.com','localhost']
 
